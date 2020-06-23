e11b220b81a8a5bd38dc5d1b447a7f8867768ae2
Aurélien Bompard <aurelien@bompard.org>
2020-06-23 19:12:06 +0200

    Prettier colors
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fas2ipa/cli.py
+++ fas2ipa/cli.py
@@ -79,11 +79,11 @@ def print_status(status, text=None):
     if status == Status.ADDED:
         color = Style.BRIGHT + Fore.GREEN
     elif status == Status.UPDATED:
-        color = Fore.GREEN
+        color = Style.BRIGHT + Fore.CYAN
     elif status == Status.FAILED:
         color = Style.BRIGHT + Fore.RED
     elif status == Status.SKIPPED:
-        color = Style.BRIGHT + Fore.BLACK
+        color = Style.BRIGHT + Fore.BLUE
     else:
         raise ValueError(f"Unknown status: {status!r}")
     print(f"{color}{text or status.value}{Style.RESET_ALL}")
