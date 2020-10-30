d9117967320abe1d638a6ba4ac6afa5ae8a80052
Nils Philippsen <nils@redhat.com>
2020-10-30 14:12:21 +0100

    Add load_data(), save_data() utility functions
    These allow loading/saving dictionary objects from/to JSON, YAML, TOML
    files.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- fas2ipa/utils.py
+++ fas2ipa/utils.py
@@ -1,6 +1,10 @@
+import json
+import pathlib
 import random
+from typing import Union
 
 import click
+import toml
 
 
 # def chunks(data, n):
@@ -31,3 +35,58 @@ class ObjectManager:
     def chunks(self, items):
         size = self.config["chunks"]
         return [items[x : x + size] for x in range(0, len(items), size)]
+
+
+def load_data(fpath: Union[str, pathlib.Path]) -> dict:
+    """Load dictionary data from a JSON, YAML, or TOML file.
+
+    The file format will be determined from the extension of the file name.
+
+    :param fpath:   The file path from which to load.
+
+    :return:        The loaded data as a dictionary.
+    """
+    if not isinstance(fpath, pathlib.Path):
+        fpath = pathlib.Path(fpath)
+
+    suffix = fpath.suffix.lower()
+
+    if suffix == ".toml":
+        data = toml.loads(fpath.read_text())
+    elif suffix == ".yaml":
+        import yaml
+        with fpath.open("r") as fobj:
+            data = yaml.safe_load(fobj)
+    else:
+        data = json.loads(fpath.read_text())
+
+    return data
+
+
+def save_data(data: dict, fpath: Union[str, pathlib.Path], force_overwrite: bool = False):
+    """Save a dictionary object to a JSON, YAML, or TOML file.
+
+    The file format will be determined from the extension of the file name.
+
+    :param data:            The data to be saved.
+    :param fpath:           The file path to be saved into.
+    :param force_overwrite: Whether an existing file should be overwritten.
+    """
+    if not isinstance(fpath, pathlib.Path):
+        fpath = pathlib.Path(fpath)
+
+    suffix = fpath.suffix.lower()
+
+    if force_overwrite:
+        mode = "w"
+    else:
+        mode = "x"
+
+    with fpath.open(mode) as fobj:
+        if suffix == ".toml":
+            toml.dump(data, fobj)
+        elif suffix == ".yaml":
+            import yaml
+            yaml.dump(data, fobj)
+        else:
+            json.dump(data, fobj)
